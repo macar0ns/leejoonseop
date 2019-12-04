68d257a8b12f769f7b6d655b824355189215ef82
macar0ns <58460264+macar0ns@users.noreply.github.com>
2019-12-04 17:45:07 +0900

    Update code.c

--- project/code.c
+++ project/code.c
@@ -1,23 +1,15 @@
-//
-//  main.c
-//  test
-//
-//  Created by 이준섭 on 21/09/2019.
-//  Copyright © 2019 이준섭. All rights reserved.
-//
-
 #include <stdio.h>
 
 void print_lines(void)
 {
-    printf("------------------ \n");
-    printf("------------------ \n");
+    printf("------------- \n");
+    printf("------------- \n");
 }
 int main(void)
 {
     int i;
     print_lines();
-    for (i = 0; i<5; i++)
+    for (i = 0; i<6; i++)
         printf("%d의 제곱은 %d \n", i , i*i);
     print_lines();
     return 0;
@@ -26,3 +18,4 @@ int main(void)
     
 
 }
+//수정 
